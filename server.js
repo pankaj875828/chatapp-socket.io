e6ed8375eb003f91d02d2e9a95cb95ab45f643c4
pankaj875828 <pankajgodhani99@gmail.com>
2021-03-22 23:35:21 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app)
+
+const port = process.env.PORT || 3000;
+
+http.listen(port,()=>{
+    console.log(`server running on:${port}`)
+})
+
+app.use(express.static(__dirname+'/public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html')
+})
+
+
+//server
+
+const io = require('socket.io')(http)
+
+io.on('connection',(socket)=>{
+    console.log('connected.....')
+    socket.on('message',(msg)=>{
+        socket.broadcast.emit('message',msg)
+    })
+})
